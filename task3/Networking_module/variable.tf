8b5a14b3a27f0a2d03c0016e7f38d3a1377c5478
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2024-12-08 14:55:59 +0530

    Create variable.tf

---
+++ task3/Networking_module/variable.tf
@@ -0,0 +1,22 @@
+variable region {
+  type        = string
+}
+variable vpc_cidr {
+  type        = string
+}
+variable pub_sub1 {
+  type        = string
+ 
+}
+variable pub_sub2 {
+  type        = string
+ 
+}
+variable pvt_sub1 {
+  type        = string
+
+}
+variable pvt_sub2 {
+  type        = string
+
+}
