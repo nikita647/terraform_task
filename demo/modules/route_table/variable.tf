3fd6cee01501e28a4ff3626a39b6344d51f79dcf
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2024-12-13 02:42:50 +0530

    Create variable.tf

---
+++ demo/modules/route_table/variable.tf
@@ -0,0 +1,25 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "internet_gateway_id" {
+  type = string
+}
+
+
+variable "nat_gateway_id" {
+  type = string
+}
+
+variable "public_sub-id" {
+  type = string
+}
+
+variable "pvt_sub1-id" {
+  type = string
+}
+
+variable "pvt_sub2-id" {
+  type = string
+}
+
