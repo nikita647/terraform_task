de43d034c02bd63b9691862e6cd706b8e76efa52
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2024-12-13 02:47:54 +0530

    Create variable.tf

---
+++ demo/modules/alb/variable.tf
@@ -0,0 +1,26 @@
+variable "vpc_id" {
+  type        = string
+  description = "description"
+  
+}
+
+variable "public_sg-id" {
+  type        = string
+  description = "description"
+}
+
+variable "public_sub-id" {
+  type        = string
+  description = "description"
+}
+
+
+variable "pvt_sub2-id" {
+  type        = string
+  description = "description"
+}
+
+variable "pvt_instance_id" {
+  type        = string
+  description = "description"
+}
