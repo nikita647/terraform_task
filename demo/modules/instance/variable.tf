1fc6de9fd2f1f428811c1131622ad9a03b3a0207
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2024-12-13 02:45:15 +0530

    Create variable.tf

---
+++ demo/modules/instance/variable.tf
@@ -0,0 +1,42 @@
+variable "ami_id" {
+  type        = string
+  description = "description"
+  default     = "ami-005fc0f236362e99f"
+
+}
+
+variable "size" {
+  type        = string
+  description = "description"
+  default     = "t2.micro"
+}
+
+variable "public_sub-id" {
+  type        = string
+  description = "description"
+  
+}
+
+variable "public_sg-id" {
+  type        = string
+  description = "description"
+
+}
+variable "key_name" {
+  type        = string
+  description = "description"
+  default     =  "key_new"
+}
+
+variable "private_sg-id" {
+  type        = string
+  description = "description"
+
+}
+
+variable "pvt_sub1-id" {
+  type        = string
+  description = "description"
+
+}
+
